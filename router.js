614dec9134bea836bfa6aba1c65a146937aa3a39
Voro123 <2393317074@qq.com>
2020-07-10 12:45:43 +0800

    master

--- router.js
+++ router.js
@@ -17,15 +17,16 @@ router
   // 首页第一次载入时获取一,二级目录数据对象
   .get('/api/getFileFolders', function (req, res) {
     dbHelper.DbSelect('select * from dt_h1 order by id', function (getData1) {
-      dbHelper.DbSelect('select * from dt_h2 order by id', function (getData2) {
-        var sendData = {};
+      dbHelper.DbSelect('select * from dt_h2 order by sort desc,id', function (getData2) {
+        var sendData = []
         // 遍历表h1并将其每一项的name作为数组对象的键名
-        for (obj1 of getData1) {
-          sendData[obj1['name']] = { id: obj1.id, childs: [] }
+        for (let obj_h1 of getData1) {
+          sendData.push(Object.assign(obj_h1, { childs: [] }))
+          let index = sendData.length - 1
           /* 遍历表h2,过滤parents属性为当前遍历h1表的name属性的项,
-          并将其每一项的name添加入数组对象childs属性*/
-          for (obj2 of getData2.filter(item => item['parents'] === obj1['name'])) {
-            sendData[obj1['name']].childs.push(obj2['name'])
+          并将其每一项的数据添加入数组对象childs属性*/
+          for (let obj_h2 of getData2.filter(item => item['parents'] === obj_h1['name'])) {
+            sendData[index].childs.push(obj_h2)
           }
         }
         res.send(JSON.stringify(sendData));
@@ -38,7 +39,7 @@ router
     const h1 = Tools.tosqlstr(req.query['h1'])
     const h2 = Tools.tosqlstr(req.query['h2'])
     var cmdstr = `select * from dt_h3 where parentsID=(select id from dt_h2\
-             where parents = "${h1}" and name = "${h2}") order by sort desc`
+             where parents = "${h1}" and name = "${h2}") order by sort desc,id`
     dbHelper.DbSelect(cmdstr, function (getData) {
       res.send(JSON.stringify(getData))
     })
@@ -203,7 +204,61 @@ router
     })
   })
 
-  // 改变词条顺序
+  // 改变h2词条顺序
+  .get('/api/resort/h2', function (req, res) {
+    const dragger_id = req.query['dragger_id']
+    const dropper_id = req.query['dropper_id']
+
+    var cmdstr = `select parents from dt_h2 where id=${dragger_id}`
+    dbHelper.DbSelect(cmdstr, function (getData) {
+      if (getData.length === 0) {
+        res.send(false)
+        return
+      }
+      var parents = getData[0]['parents']
+
+      // dropper_id(释放点上一个兄弟结点)为null的情况
+      if (!dropper_id) {
+        cmdstr = `select max(sort) from dt_h2 where parents="${parents}"`
+        dbHelper.DbSelect(cmdstr, function (getData) {
+          if (!getData) {
+            res.send(false)
+            return
+          }
+          cmdstr = `update dt_h2 set sort=${getData[0]['max(sort)']}+1 where id=${dragger_id}`
+          dbHelper.DbExecute(cmdstr, function (getData) {
+            res.send(getData)
+          })
+        })
+        return
+      }
+
+      // dropper_id(释放点上一个兄弟结点)不为null的情况
+      cmdstr = `select * from dt_h2 where id=${dropper_id}`
+      dbHelper.DbSelect(cmdstr, function (getData) {
+        if (getData.length === 0) {
+          res.send(false)
+          return
+        }
+        var dropper_obj = getData[0]
+        cmdstr = `update dt_h2 set sort=sort+2 where parents="${parents}" and\
+           (sort>${dropper_obj.sort} or (sort=${dropper_obj.sort}\
+             and id<=${dropper_obj.id}))`
+        dbHelper.DbExecute(cmdstr, function (getData) {
+          if (!getData) {
+            res.send(false)
+            return
+          }
+          cmdstr = `update dt_h2 set sort=${dropper_obj.sort}+1 where id=${dragger_id}`
+          dbHelper.DbExecute(cmdstr, function (getData) {
+            res.send(getData)
+          })
+        })
+      })
+    })
+  })
+
+  // 改变h3词条顺序
   .get('/api/resort/h3', function (req, res) {
     const dragger_id = req.query['dragger_id']
     const dropper_id = req.query['dropper_id']
@@ -449,6 +504,37 @@ router
     let cmdstr = `select * from dt_h3 where noappear=1`
     dbHelper.DbSelect(cmdstr, function (getData) {
       res.send(getData)
+    })
+  })
+
+  // 测试用api,使用该api将在test/test目录下创建n个实验词条并将原来的词条清空
+  .get('/api/test', function (req, res) {
+    let cmdstr = `select id from dt_h2 where name="test" and parents="test"`
+    dbHelper.DbSelect(cmdstr, function (getData) {
+      if (!getData || getData.length === 0) {
+        res.send('失败,请确认test/test目录存在')
+        return
+      }
+      let parentsID = getData[0]['id']
+      cmdstr = `delete from dt_h3 where parentsID=${parentsID}`
+      dbHelper.DbExecute(cmdstr, function (getData) {
+        if (!getData) {
+          res.send('defeat')
+          return
+        }
+        let strvalues = `("test0",${parentsID})`
+        for (let i = 1; i < 1000; i++) {
+          strvalues += `,("test${i}",${parentsID})`
+        }
+        cmdstr = `insert into dt_h3(name,parentsID) values${strvalues}`
+        dbHelper.DbExecute(cmdstr, function (getData) {
+          if (!getData) {
+            res.send('defeat')
+            return
+          }
+          res.send('success')
+        })
+      })
     })
   })
 
